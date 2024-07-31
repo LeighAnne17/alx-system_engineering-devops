0e49ff2f55c349277819b180da845b3345f8b060
LeighAnne17 <leighndimande17@icloud.com>
2024-07-31 14:45:39 -0700

    api

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,45 +1,31 @@
 #!/usr/bin/python3
-import requests
-import sys
+""" Extends task 1 to export data in the JSON format """
+from requests import get
+from sys import argv
 import json
 
-def export_to_json(employee_id):
-    # Base URL of the API
-    base_url = 'https://jsonplaceholder.typicode.com'
-    
-    # Get employee information
-    user_response = requests.get(f'{base_url}/users/{employee_id}')
-    user_data = user_response.json()
-    username = user_data.get('username')
 
-    # Get employee TODO list
-    todos_response = requests.get(f'{base_url}/todos', params={'userId': employee_id})
-    todos_data = todos_response.json()
+def to_json(employee_id):
+    """ Uses get to pull data and exports data in a JSON format """
+    users = get("https://jsonplaceholder.typicode.com/users/" +
+                employee_id).json()
+    todos = get("https://jsonplaceholder.typicode.com/todos?userId=" +
+                employee_id).json()
+    if not users or not todos:
+        return ("Not a valid JSON")
+    task_list = []
+    for task in todos:
+        task_dict = {}
+        task_dict['task'] = task.get('title')
+        task_dict['completed'] = task.get('completed')
+        task_dict['username'] = users.get('username')
+        task_list.append(task_dict)
+    data = {}
+    data[employee_id] = task_list
+    with open('{}.json'.format(employee_id), 'w') as file:
+        json.dump(data, file)
 
-    # Prepare data for JSON export
-    tasks = []
-    for task in todos_data:
-        task_info = {
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": username
-        }
-        tasks.append(task_info)
-    
-    # Define the filename
-    filename = f"{employee_id}.json"
 
-    # Write data to JSON file
-    with open(filename, 'w') as file:
-        json.dump({str(employee_id): tasks}, file, indent=4)
-
-    print(f"Data exported to {filename}")
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python3 2-export_to_JSON.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    export_to_json(employee_id)
-
+if __name__ == "__main__":
+    if len(argv) > 1:
+        to_json(argv[1])
