5828dbe1f260d5ebd8556e0c074bfa4d4d1a799f
LeighAnne17 <leighndimande17@icloud.com>
2024-07-31 14:42:50 -0700

    api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-""" Python script that uses REST API to returns information about a given
-employee's TODO list progress """
+""" Extends task 1 to export data in the JSON format """
 from requests import get
 from sys import argv
+import json
 
 
-def get_todos(employee_id):
-    """ Uses get to pull todo tasks of passed in employee_id """
-    users = get("https://jsonplaceholder.typicode.com/users/" +
-                employee_id).json()
-    todos = get("https://jsonplaceholder.typicode.com/todos?userId=" +
-                employee_id).json()
+def all_to_json():
+    """ Uses get to pull data and exports data in a CSV format """
+    users = get("https://jsonplaceholder.typicode.com/users/").json()
+    todos = get("https://jsonplaceholder.typicode.com/todos").json()
     if not users or not todos:
         return ("Not a valid JSON")
-    name = users.get('name')
-    completed = [task.get('title') for task in todos if task.get('completed')]
-    total_tasks_count = len(todos)
-    completed_count = len(completed)
-    print("Employee {} is done with tasks({}/{}):".format(name,
-                                                          completed_count,
-                                                          total_tasks_count))
-    for t in completed:
-        print("\t {}".format(t))
+    user_dict = {}
+    username_dict = {}
+    for user in users:
+        employee_id = user.get('id')
+        user_dict[employee_id] = []
+        username_dict[employee_id] = user.get('username')
+    for task in todos:
+        task_dict = {}
+        employee_id = task.get('userId')
+        task_dict['task'] = task.get('title')
+        task_dict['completed'] = task.get('completed')
+        task_dict['username'] = username_dict.get(employee_id)
+        user_dict.get(employee_id).append(task_dict)
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(user_dict, file)
 
 
 if __name__ == "__main__":
-    if len(argv) > 1:
-        get_todos(argv[1])
+    all_to_json()
