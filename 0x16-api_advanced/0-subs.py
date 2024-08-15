62460dc63712a47c6a444b4a740ebbba745a96d7
LeighAnne17 <leighndimande17@icloud.com>
2024-08-15 14:59:53 -0700

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,13 @@ queries the api and returns number of subs for a given subreddit
 import requests
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json().get("data", {})
-        return data.get("subscribers", 0)
+        data = response.json()
+        subcribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
-
