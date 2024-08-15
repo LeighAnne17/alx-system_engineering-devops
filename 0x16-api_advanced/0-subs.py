a565a98ebac19c2373b54873b79bd05c256e64b4
LeighAnne17 <leighndimande17@icloud.com>
2024-08-15 14:15:39 -0700

    api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+queries the api and returns number of subs for a given subreddit
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json().get("data", {})
+        return data.get("subscribers", 0)
+    else:
+        return 0
