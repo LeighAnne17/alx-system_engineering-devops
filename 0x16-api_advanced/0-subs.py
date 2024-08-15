63e81446268162ab35783ed2b349f9fc19072421
LeighAnne17 <leighndimande17@icloud.com>
2024-08-15 14:34:34 -0700

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Reddit-API-Checker/0.1"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json().get("data", {})
         return data.get("subscribers", 0)
     else:
-        print(f"Error: {response.status_code}")
-        print(f"Response: {response.text}")
         return 0
+
